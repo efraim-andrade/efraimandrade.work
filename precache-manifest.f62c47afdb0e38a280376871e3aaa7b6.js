0f1f83842d0bb5fcaa282bc77a37b7e4f7bcee0f
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2019-04-21 13:34:41 -0300

    Updates

---
+++ precache-manifest.f62c47afdb0e38a280376871e3aaa7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c490b729e7abeaa9c0d7",
+    "url": "/static/js/main.fb3c6e30.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "c490b729e7abeaa9c0d7",
+    "url": "/static/css/main.97a5df0b.chunk.css"
+  },
+  {
+    "revision": "df010fe41edf68a5b8c6ea40cc16d0b1",
+    "url": "/index.html"
+  }
+];
