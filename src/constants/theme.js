e461371c7acf509b7fab009326291bf9f7a4124d
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2019-04-21 15:41:33 -0300

    feat(geral): estruturando o projeto

---
+++ src/constants/theme.js
@@ -0,0 +1,20 @@
+const colors = {
+    primary: "#282A36",
+
+    //Dracula
+    equal: "#FF76C7",
+    string: "#F1FC85",
+    attribute: "#45F96E",
+    component: "#87E9FE",
+    background: "#17181F",
+
+    //Mac
+    danger: "#F95F57",
+    success: "#2BC740",
+    warning: "#FFBF32",
+
+
+    white: "#FFF",
+}
+
+export { colors }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { colors } from '../../constants';
+
+export const Container = styled.div`
+    background-color: ${colors.component}
+`;
