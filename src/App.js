b7c70e57b9bbf1a340f7b3e35eb66022e3a017d2
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2019-04-22 02:02:44 -0300

    feat(geral): google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 import Home from './pages/Home/index';
 
@@ -6,6 +7,11 @@ import GlobalStyle from './styles';
 import './styles/fonts.css';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-138749706-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
+
   return (
     <>
       <GlobalStyle />
